70acc0872e2aff6ff64dd28c5062eef8532407a1
Elias Kuiter <info@elias-kuiter.de>
2025-02-21 17:40:25 +0100

    added penguin grid

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -28,32 +28,64 @@ create-sample(n) {
     docker run --rm --platform linux/amd64 tikzpingurs "$n" | grep -v \\.$
 }
 
+render(tex, target) {
+    latexmk=(docker run --rm -v "$PWD"/scripts:/home -w /home texlive/texlive latexmk)
+    "${latexmk[@]}" -quiet -silent -pdf -pdflatex='pdflatex -interaction=batchmode -halt-on-error' "$tex" >/dev/null
+    mv "$SCRIPTS_DIRECTORY"/"$tex".pdf "$target"
+    "${latexmk[@]}" -quiet -silent -C "$tex" >/dev/null 2>&1
+}
+
 # render all the penguins in the sample as PDF files
-render-penguins(sample) {
+render-each(sample) {
+    if [[ ! -f "$sample" ]]; then
+        error "Penguin sample not generated yet."
+    fi
+    i=1
+    while read -r configuration; do
+        echo "Rendering penguin #$i ..."
+        sed "s/#body#/${configuration//\\/\\\\}/" "$SCRIPTS_DIRECTORY"/template.tex > "$SCRIPTS_DIRECTORY"/template.gen.tex
+        sed -i "s/#options#//" "$SCRIPTS_DIRECTORY"/template.gen.tex
+        render template.gen "$(dirname "$sample")"/$i.pdf
+        i=$((i+1))
+    done < "$sample"
+}
+
+# render a grid of all the penguins in the sample
+render-grid(sample, columns) {
     if [[ ! -f "$sample" ]]; then
         error "Penguin sample not generated yet."
     fi
     i=1
     latexmk=(docker run --rm -v "$PWD"/scripts:/home -w /home texlive/texlive latexmk)
+    cp "$SCRIPTS_DIRECTORY"/template.tex "$SCRIPTS_DIRECTORY"/template.gen.tex
+    if [[ $columns -gt 0 ]]; then
+        # this is currently a hack because varwidth does not work correctly
+        sed -i "s/#options#/margin={0 0 60pt 0},varwidth/" "$SCRIPTS_DIRECTORY"/template.gen.tex
+    else
+        sed -i "s/#options#//" "$SCRIPTS_DIRECTORY"/template.gen.tex
+    fi
     while read -r configuration; do
-        echo "Rendering penguin #$i ..."
-        sed "s/#/${configuration//\\/\\\\}/" "$SCRIPTS_DIRECTORY"/template.tex > "$SCRIPTS_DIRECTORY"/template.gen.tex
-        "${latexmk[@]}" -quiet -silent -pdf -pdflatex='pdflatex -interaction=batchmode -synctex=1 -halt-on-error' template.gen.tex >/dev/null
-        mv "$SCRIPTS_DIRECTORY"/template.gen.pdf "$(dirname "$sample")"/$i.pdf
-        "${latexmk[@]}" -quiet -silent -C template.tex >/dev/null 2>&1
-        "${latexmk[@]}" -quiet -silent -C template.gen.tex >/dev/null 2>&1
-        rm -rf "$SCRIPTS_DIRECTORY"/template.gen.*
+        sed -i "s/#body#/${configuration//\\/\\\\}#body#/" "$SCRIPTS_DIRECTORY"/template.gen.tex
+        if [[ $columns -gt 0 && $((i % columns)) -eq 0 ]]; then
+            sed -i "s/#body#/\\\\\\\\#body#/" "$SCRIPTS_DIRECTORY"/template.gen.tex
+        fi
         i=$((i+1))
     done < "$sample"
+    sed -i "s/#body#//" "$SCRIPTS_DIRECTORY"/template.gen.tex
+    echo "Rendering penguin grid ..."
+    render template.gen "$(dirname "$sample")"/grid.pdf
 }
 
-run(n=5) {
+run(n=30, columns=5) {
     rm -rf "$SCRIPTS_DIRECTORY"/../build
     mkdir -p "$SCRIPTS_DIRECTORY"/../build
 
     transform-model > "$SCRIPTS_DIRECTORY"/penguin.xml
     create-sample "$n" > "$SCRIPTS_DIRECTORY"/../build/sample.tex
 
-    render-penguins "$SCRIPTS_DIRECTORY"/../build/sample.tex
-    docker run --rm -v "$PWD"/build:/work/files --platform linux/amd64 ghcr.io/bogosj/merge-pdfs
+    # render-each "$SCRIPTS_DIRECTORY"/../build/sample.tex
+    # docker run --rm -v "$PWD"/build:/work/files --platform linux/amd64 ghcr.io/bogosj/merge-pdfs
+    # mv "$SCRIPTS_DIRECTORY"/../build/output.pdf "$SCRIPTS_DIRECTORY"/../build/all.pdf
+
+    render-grid "$SCRIPTS_DIRECTORY"/../build/sample.tex "$columns"
 }
